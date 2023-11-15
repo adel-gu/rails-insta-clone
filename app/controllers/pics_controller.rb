447bfd2b6dd22aacb4fe8c57763dd5e1a4ffe03b
adel <adeelguitoun@gmail.com>
2023-11-15 14:33:29 +0100

    Add edit and delete pic

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,9 +1,8 @@
 class PicsController < ApplicationController
-  before_action :find_pic, only: [:show]
-
-  # , :edit, :update, :destroy
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show; end
@@ -19,6 +18,24 @@ class PicsController < ApplicationController
       redirect_to @pic, notice: 'Yesss! It was posted'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: 'Congrats! Pic was updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @pic.destroy
+      redirect_to root_path, notice: 'Pic was deleted successfully!'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
